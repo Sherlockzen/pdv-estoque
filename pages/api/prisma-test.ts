8fe792cfad493bd33df447cceca921d499b01bbe
Hewerton <hewertonlourenco@gmail.com>
2023-08-12 23:06:37 -0300

    Chore: testing api with prisma in deploy

--- pages/api/prisma-test.ts
+++ pages/api/prisma-test.ts
@@ -7,12 +7,12 @@ export default async function prismaExample(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'Zezo Soares',
-      email: 'zezo@example.com',
-    }
-  })
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: 'Zezo Soares',
+  //     email: 'zezo@example.com',
+  //   }
+  // })
  
   const users = await prisma.user.findMany();
   return response.status(200).json({ users });
