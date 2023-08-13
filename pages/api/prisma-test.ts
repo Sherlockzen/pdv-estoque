42d26ce3078c7ccec797e3e7b1a2221af851167f
Hewerton <hewertonlourenco@gmail.com>
2023-08-12 22:48:15 -0300

    Feat: initial boilerplate add prisma & postgres

---
+++ pages/api/prisma-test.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+ 
+const prisma = new PrismaClient();
+ 
+export default async function prismaExample(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Zezo Soares',
+      email: 'zezo@example.com',
+    }
+  })
+ 
+  const users = await prisma.user.findMany();
+  return response.status(200).json({ users });
+}
